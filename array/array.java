9dfd7e5dbe906252ce62ccd8bbf48d90ad7bc366
kotatsuko <107920100+kotatsuko@users.noreply.github.com>
2022-10-29 06:56:03 +0000

    20221029

---
+++ array/array.java
@@ -0,0 +1,10 @@
+public class array {
+  public static void main(String[] args) {
+
+    int[] num = new int[ 0]; 
+    System.out.println(num.getClass()); 
+    double[] num2 = new double[ 0]; 
+    System.out.println(num2.getClass());
+
+  }
+}
